f6ce22f822a2adb00953ed1b3e5a2d9a0b4355f0
BasantJoshi6363 <120437433+BasantJoshi6363@users.noreply.github.com>
2024-03-16 22:04:51 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,12 +10,7 @@ dotenv.config({
 let app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin:["https://full-stack-todo-app-wvfc.vercel.app"],
-  methods : ["POST","GET"],
-  credentials : true
-  }
-));
+app.use(cors())
 app.use("/", taskRouter);
 let port = process.env.PORT;
 connectionTODatabase();
