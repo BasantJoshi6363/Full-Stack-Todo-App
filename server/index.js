79649ef17e24b478909396b89373dda394a1a522
BasantJoshi6363 <120437433+BasantJoshi6363@users.noreply.github.com>
2024-03-16 22:10:03 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,17 +3,20 @@ import cors from "cors";
 import { connectionTODatabase } from "./src/database/connection.js";
 import taskRouter from "./src/database/task.routes.js";
 import dotenv from "dotenv";
+
 dotenv.config({
   path: "./env",
 });
 
-let app = express();
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/", taskRouter);
-let port = process.env.PORT;
-connectionTODatabase();
+
+connectionTODatabase(); // Ensure this connects to your database
+
 app.listen(port, () => {
-  console.log("connected to server");
+  console.log(`Server connected and listening on port ${port}`);
 });
